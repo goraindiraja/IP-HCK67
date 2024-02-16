9d9c8da60d6b1828ef0b9c409c839e488eeaaaf1
Gora Indiraja Shafa <gora.indiraja@gmail.com>
2024-02-16 10:08:58 +0700

    test: Server - Create Testing Cases For PUT and PATCH /users/:id

--- server/__tests__/users.test.js
+++ server/__tests__/users.test.js
@@ -1,4 +1,5 @@
 const app = require("../app");
+const fs = require('fs');
 const request = require("supertest");
 const { sequelize, User} = require("../models");
 const { queryInterface } = sequelize;
@@ -74,7 +75,7 @@ describe("GET /users/:id", () => {
     })
 
     describe("Failed", () => {
-        test.only("GET Users by Id Failed (Invalid Token): Should return status 401 and Error Message", async () => {
+        test("GET Users by Id Failed (Invalid Token): Should return status 401 and Error Message", async () => {
             let id = 1
             let {status, body} = await request(app)
                 .get(`/users/${id}`)
@@ -84,7 +85,7 @@ describe("GET /users/:id", () => {
             expect(body).toHaveProperty("message", "Unauthenticated")
         })
 
-        test.only("GET Users Failed (Not Login): Should return status 401 and Error Message", async () => {
+        test("GET Users Failed (Not Login): Should return status 401 and Error Message", async () => {
             let id = 1
             let {status, body} = await request(app)
                 .get(`/users/${id}`)
@@ -93,7 +94,7 @@ describe("GET /users/:id", () => {
             expect(body).toHaveProperty("message", "Unauthenticated")
         })
 
-        test.only("GET Users Failed (Not Found): Should return status 401 and Error Message", async () => {
+        test("GET Users Failed (Not Found): Should return status 401 and Error Message", async () => {
             let id = 100
             let {status, body} = await request(app)
                 .get(`/users/${id}`)
@@ -101,6 +102,166 @@ describe("GET /users/:id", () => {
                 
             expect(status).toBe(404)
             expect(body).toHaveProperty("message", "Data Not Found")
+        })
+    })
+})
+
+describe("PUT /users/:id", () => {
+    describe("Success", () => {
+        test("Update User Success: Should return status 200 and updated data", async () => {
+            let id = 1
+            let updatedData = {
+                name: "King",
+                email: "king@test.com"
+            }
+
+            let {status, body} = await request(app)
+                .put(`/users/${id}`)
+                .set("Authorization", `Bearer ${access_token}`)
+                .send(updatedData)
+
+            expect(status).toBe(200)
+            expect(body).toHaveProperty("message", "Data 1 has been updated")
+        })
+    })
+
+    describe("Success", () => {
+        test("Update User Failed (Name is empty): Should return status 400 and Error Message", async () => {
+            let id = 1
+            let updatedData = {
+                name: " ",
+                email: "king@test.com"
+            }
+
+            let {status, body} = await request(app)
+                .put(`/users/${id}`)
+                .set("Authorization", `Bearer ${access_token}`)
+                .send(updatedData)
+
+            expect(status).toBe(400)
+            expect(body).toHaveProperty("message", "Name is required")
+        })
+
+        test("Update User Failed (Email is empty): Should return status 400 and Error Message", async () => {
+            let id = 1
+            let updatedData = {
+                name: "King",
+                email: " "
+            }
+
+            let {status, body} = await request(app)
+                .put(`/users/${id}`)
+                .set("Authorization", `Bearer ${access_token}`)
+                .send(updatedData)
+
+            expect(status).toBe(400)
+            expect(body).toHaveProperty("message", "Email is required")
+        })
+
+        test.only("Update User Failed (Invalid Email Format): Should return status 400 and Error Message", async () => {
+            let id = 1
+            let updatedData = {
+                name: "King",
+                email: "King.com"
+            }
+
+            let {status, body} = await request(app)
+                .put(`/users/${id}`)
+                .set("Authorization", `Bearer ${access_token}`)
+                .send(updatedData)
+
+            expect(status).toBe(400)
+            expect(body).toHaveProperty("message", "Invalid Email Format")
+        })
+
+        test("Update User Failed (Invalid Token): Should return status 401 and Error Message", async () => {
+            let id = 1
+            let updatedData = {
+                name: "King",
+                email: "king.@test.com"
+            }
+
+            let {status, body} = await request(app)
+                .put(`/users/${id}`)
+                .set("Authorization", `Bearer ${access_token_false}`)
+                .send(updatedData)
+
+            expect(status).toBe(401)
+            expect(body).toHaveProperty("message", "Unauthenticated")
+        })
+
+        test("Update User Failed (Not Login): Should return status 401 and Error Message", async () => {
+            let id = 1
+            let updatedData = {
+                name: "King",
+                email: "king.@test.com"
+            }
+
+            let {status, body} = await request(app)
+                .put(`/users/${id}`)
+                .send(updatedData)
+
+            expect(status).toBe(401)
+            expect(body).toHaveProperty("message", "Unauthenticated")
+        })
+
+        test("Update User Failed (Data Not Found): Should return status 401 and Error Message", async () => {
+            let id = 100
+            let updatedData = {
+                name: "King",
+                email: "king.@test.com"
+            }
+
+            let {status, body} = await request(app)
+                .put(`/users/${id}`)
+                .send(updatedData)
+
+            expect(status).toBe(404)
+            expect(body).toHaveProperty("message", "Data Not Found")
+        })
+    })
+})
+
+describe("PATCH /users/:id/img", () => {
+    describe("Success", () => {
+        test("Patch Image Success: Should return status 200 and Success Message", async () =>{
+            let id = 1
+            let image_sample = fs.readFileSync('./uploads/Fern.jpeg')
+            let {status, body} = await request(app)
+                .patch(`/users/${id}/img`)
+                .set("Authorization", `Bearer ${access_token}`)
+                .attach('image', image_sample, {filename: 'Fern.jpeg'})
+                
+                console.log(body);
+                expect(status).toBe(200)
+                expect(body).toHaveProperty("message", `Image User ${id} has been updated`)
+        })
+    })
+
+    describe("Failed", () => {
+        test("Patch Image Failed (Invalid Token): Should return status 200 and Success Message", async () =>{
+            let id = 1
+            let image_sample = fs.readFileSync('./uploads/Fern.jpeg')
+            let {status, body} = await request(app)
+                .patch(`/users/${id}/img`)
+                .set("Authorization", `Bearer ${access_token_false}`)
+                .attach('image', image_sample, {filename: 'Fern.jpeg'})
+                
+                console.log(body);
+                expect(status).toBe(401)
+                expect(body).toHaveProperty("message", `Unauthenticated`)
+        })
+
+        test("Patch Image Failed (Not Login): Should return status 200 and Success Message", async () =>{
+            let id = 1
+            let image_sample = fs.readFileSync('./uploads/Fern.jpeg')
+            let {status, body} = await request(app)
+                .patch(`/users/${id}/img`)
+                .attach('image', image_sample, {filename: 'Fern.jpeg'})
+                
+                console.log(body);
+                expect(status).toBe(401)
+                expect(body).toHaveProperty("message", `Unauthenticated`)
         })
     })
 })
